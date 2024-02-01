bb341bebb868f61847fd7fa9738b65bff43b7a5f
Harsh Kamde <105597593+harsh-kamde@users.noreply.github.com>
2024-02-01 22:23:59 +0530

    Update server.js
    final cors issue solve

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 
 // lets solve cors policy 
 const corsOptions = {
-    origin:"https://sihfrontend-24kn6qvnk-harsh-kamdes-projects.vercel.app",
+    origin:"https://sihfrontend.vercel.app",
     methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials:true,
 }
